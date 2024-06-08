4ec1ffb6d39b8090b1f94abab55f4315e6832800
kkaiyu <127333335+kkaiyu@users.noreply.github.com>
2024-06-09 00:30:09 +0800

    combine date & time llm

--- flow/datetimeexamples.py
+++ flow/datetimeexamples.py
@@ -1,5 +1,5 @@
 from promptflow import tool
-import json
+# import json
 
 # The inputs section will change based on the arguments of the tool function, after you save the code
 # Adding type to arguments and return value will help the system show the types properly
@@ -18,21 +18,21 @@ def date_examples():
             "input2": "Singapore “strongly condemns” the attack that took place at a police station in Johor on April 7, said the Ministry of Foreign Affairs (MFA) in a statement.",
             "output": "Input1: Singapore “strongly condemns” the attack that took place at a police station in Johor on May 17, said the Ministry of Foreign Affairs (MFA) in a statement.\n\n" +
             "Input2: Singapore “strongly condemns” the attack that took place at a police station in Johor on April 7, said the Ministry of Foreign Affairs (MFA) in a statement.\n\n" + 
-            json.dumps(
+            # json.dumps(
                 {"Date": [
                     {"Sentence1": "Singapore “strongly condemns” the attack that took place at a police station in Johor on May 17, said the Ministry of Foreign Affairs (MFA) in a statement.",
                     "Sentence2": "Singapore “strongly condemns” the attack that took place at a police station in Johor on April 7, said the Ministry of Foreign Affairs (MFA) in a statement.",
                     "Reason": "The date of the attack in Sentence2 was on April 7, whereas the date in Sentence1 was on May 17."
                     }]
                 }
-            )
+            # )
         },
         {
             "input1": "More than 160 automated lanes were installed in 2023, with another 230 lanes to be set up in 2024. ICA intends to install about 800 such lanes across all of Singapore’s checkpoints by 2026.",
             "input2": "More than 160 automated lanes were installed in 2015, with another 230 lanes to be set up in 2018. ICA intends to install about 800 such lanes across all of Singapore’s checkpoints by 2020.",
             "output": "Input1: More than 160 automated lanes were installed in 2023, with another 230 lanes to be set up in 2024. ICA intends to install about 800 such lanes across all of Singapore’s checkpoints by 2026.\n\n" +
             "Input2: More than 160 automated lanes were installed in 2015, with another 230 lanes to be set up in 2018. ICA intends to install about 800 such lanes across all of Singapore’s checkpoints by 2020.\n\n" +
-            json.dumps(
+            # json.dumps(
                 {"Date": [
                     {"Sentence1": "More than 160 automated lanes were installed in 2023, with another 230 lanes to be set up in 2024.",
                     "Sentence2": "More than 160 automated lanes were installed in 2015, with another 230 lanes to be set up in 2018.",
@@ -45,6 +45,6 @@ def date_examples():
                     }
                 ]
                 }
-            )
+            # )
         }
     ]
