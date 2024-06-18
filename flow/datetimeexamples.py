175173134599bc52ecbc09137968c09c6cbb9a9b
kkaiyu <127333335+kkaiyu@users.noreply.github.com>
2024-06-18 10:29:48 +0800

    update object flow

--- flow/datetimeexamples.py
+++ flow/datetimeexamples.py
@@ -8,30 +8,24 @@ import json
 def datetime_examples():
     return [
         {
-            "input1": "Some opposition parties congratulated Mr Lawrence Wong ahead of his swearing-in as prime minister at the Istana on May 15, while thanking outgoing prime minister Lee Hsien Loong for his decades of public service and leadership.", 
-            "input2": "Some opposition parties congratulated Mr Lawrence Wong ahead of his swearing-in as prime minister at the Istana on May 15, while thanking outgoing prime minister Lee Hsien Loong for his decades of public service and leadership.",
-            "output": "Input1: Some opposition parties congratulated Mr Lawrence Wong ahead of his swearing-in as prime minister at the Istana on May 15, while thanking outgoing prime minister Lee Hsien Loong for his decades of public service and leadership.\n\n" +
-            "Input2: Some opposition parties congratulated Mr Lawrence Wong ahead of his swearing-in as prime minister at the Istana on May 15, while thanking outgoing prime minister Lee Hsien Loong for his decades of public service and leadership."
-        },
-        {
-            "input1": "Singapore “strongly condemns” the attack that took place at a police station in Johor on May 17, said the Ministry of Foreign Affairs (MFA) in a statement.",
-            "input2": "Singapore “strongly condemns” the attack that took place at a police station in Johor on April 7, said the Ministry of Foreign Affairs (MFA) in a statement.",
-            "output": "Input1: Singapore “strongly condemns” the attack that took place at a police station in Johor on May 17, said the Ministry of Foreign Affairs (MFA) in a statement.\n\n" +
-            "Input2: Singapore “strongly condemns” the attack that took place at a police station in Johor on April 7, said the Ministry of Foreign Affairs (MFA) in a statement.\n\n" + 
+            "input1": "City now have the chance of winning the double when they face Manchester United in the FA Cup final on May 25.",
+            "input2": "City now have the chance of winning the double when they face Liverpool in the FA Cup final on May 20.",
+            "output": "Input1: City now have the chance of winning the double when they face Manchester United in the FA Cup final on May 25." +
+            "Input2: City now have the chance of winning the double when they face Liverpool in the FA Cup final on May 20." +
             json.dumps(
                 {"DateTime": [
-                    {"Sentence1": "Singapore “strongly condemns” the attack that took place at a police station in Johor on May 17, said the Ministry of Foreign Affairs (MFA) in a statement.",
-                    "Sentence2": "Singapore “strongly condemns” the attack that took place at a police station in Johor on April 7, said the Ministry of Foreign Affairs (MFA) in a statement.",
-                    "Reason": "The date of the attack in Sentence2 (April 7) does not match the date in Sentence1 (May 17)."
+                    {"Sentence1": "City now have the chance of winning the double when they face Manchester United in the FA Cup final on May 25.",
+                    "Sentence2": "City now have the chance of winning the double when they face Liverpool in the FA Cup final on May 20.",
+                    "Reason": "The date of the FA Cup final in Sentence2 (May 20) does not match with the date in Sentence1 (May 25)."
                     }]
                 }
             )
         },
         {
             "input1": "More than 160 automated lanes were installed in 2023, with another 230 lanes to be set up in 2024. ICA intends to install about 800 such lanes across all of Singapore’s checkpoints by 2026.",
-            "input2": "More than 160 automated lanes were installed in 2015, with another 230 lanes to be set up in 2018. ICA intends to install about 800 such lanes across all of Singapore’s checkpoints by 2020.",
+            "input2": "More than 160 automated lanes were installed in 2015, with another 230 lanes to be set up in 2018. BDF intends to install about 800 such lanes across all of Malaysia’s checkpoints by 2020.",
             "output": "Input1: More than 160 automated lanes were installed in 2023, with another 230 lanes to be set up in 2024. ICA intends to install about 800 such lanes across all of Singapore’s checkpoints by 2026.\n\n" +
-            "Input2: More than 160 automated lanes were installed in 2015, with another 230 lanes to be set up in 2018. ICA intends to install about 800 such lanes across all of Singapore’s checkpoints by 2020.\n\n" +
+            "Input2: More than 160 automated lanes were installed in 2015, with another 230 lanes to be set up in 2018. BDF intends to install about 800 such lanes across all of Malaysia’s checkpoints by 2020.\n\n" +
             json.dumps(
                 {"DateTime": [
                     {"Sentence1": "More than 160 automated lanes were installed in 2023, with another 230 lanes to be set up in 2024.",
@@ -40,7 +34,7 @@ def datetime_examples():
                     },
 
                     {"Sentence1": "ICA intends to install about 800 such lanes across all of Singapore’s checkpoints by 2026.",
-                    "Sentence2": "ICA intends to install about 800 such lanes across all of Singapore’s checkpoints by 2020.",
+                    "Sentence2": "BDF intends to install about 800 such lanes across all of Malaysia’s checkpoints by 2020.",
                     "Reason": "Sentence2 states the installation will be completed by 2026, whereas Sentence2 states it will be completed by 2020."
                     }
                 ]
@@ -49,28 +43,14 @@ def datetime_examples():
         },
         {
             "input1": "The community of Willetton in Perth’s south was shaken after reports surfaced on local news platforms. The incident reportedly took place in High Street before 11.30pm on May 4.",
-            "input2": "The community of Willetton in Perth’s south was shaken after reports surfaced on local news platforms. The incident reportedly took place in High Street before 12.45pm on June 19.",
+            "input2": "The community of Willetton in Sydney’s south was shaken after reports surfaced on local news platforms. The incident reportedly took place in Low Street before 12.45pm on June 19.",
             "output": "Input1: The community of Willetton in Perth’s south was shaken after reports surfaced on local news platforms. The incident reportedly took place in High Street before 11.30pm on May 4.\n\n" +
-            "Input2: The community of Willetton in Perth’s south was shaken after reports surfaced on local news platforms. The incident reportedly took place in High Street before 12.45pm on June 19.\n\n" +
+            "Input2: The community of Willetton in Sydney’s south was shaken after reports surfaced on local news platforms. The incident reportedly took place in Low Street before 12.45pm on June 19.\n\n" +
             json.dumps(
                 {"DateTime": [
                     {"Sentence1": "The incident reportedly took place in High Street before 11.30pm on May 4.",
-                    "Sentence2": "The incident reportedly took place in High Street before 12.45pm on June 19.",
+                    "Sentence2": "The incident reportedly took place in Low Street before 12.45pm on June 19.",
                     "Reason": "The date and time of the incident in Sentence2 (June 19, 12.45pm) does not match with the date and time in Sentence1 (May 4, 11.30pm)."
-                    }]
-                }
-            )
-        },
-        {
-            "input1": "City now have the chance of winning the double when they face Manchester United in the FA Cup final on May 25.",
-            "input2": "On 20 May they will aim to become the first side to complete the domestic Treble in successive seasons when they face Liverpool in the FA Cup final at Wembley.",
-            "output": "Input1: City now have the chance of winning the double when they face Manchester United in the FA Cup final on May 25." +
-            "Input2: On 20 May they will aim to become the first side to complete the domestic Treble in successive seasons when they face Liverpool in the FA Cup final at Wembley." +
-            json.dumps(
-                {"DateTime": [
-                    {"Sentence1": "City now have the chance of winning the double when they face Manchester United in the FA Cup final on May 25.",
-                    "Sentence2": "On 20 May they will aim to become the first side to complete the domestic Treble in successive seasons when they face Liverpool  in the FA Cup final at Wembley.",
-                    "Reason": "The date of the FA Cup final in Sentence2 (May 20) does not match with the date in Sentence1 (May 25)."
                     }]
                 }
             )
