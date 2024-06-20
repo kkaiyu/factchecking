46da34a14d3dd9f17af34e047612a401b5cf57b4
kkaiyu <127333335+kkaiyu@users.noreply.github.com>
2024-06-20 16:57:07 +0800

    streamlit output

--- flow/locationexamples.py
+++ flow/locationexamples.py
@@ -23,6 +23,20 @@ def location_examples():
             )
         }, 
         {
+            "input": "Input1: City now have the chance of winning the double when they face Manchester United in the FA Cup final next Saturday.\n\n" +
+            "Input2: On 25 May they will aim to become the first side to complete the domestic Treble in successive seasons when they face Liverpool in the FA Cup final at Wembley.",
+            "output": "Input1: City now have the chance of winning the double when they face Manchester United in the FA Cup final next Saturday.\n\n" +
+            "Input2: On 25 May they will aim to become the first side to complete the domestic Treble in successive seasons when they face Liverpool in the FA Cup final at Wembley.\n\n" +
+            json.dumps(
+                {"Location": [
+                    {"Sentence1": "City now have the chance of winning the double when they face Manchester United in the FA Cup final next Saturday.",
+                    "Sentence2": "On 25 May they will aim to become the first side to complete the domestic Treble in successive seasons when they face Liverpool in the FA Cup final at Wembley.",
+                    "Reason": "The location of the FA Cup final in Sentence2 (Wembley) is not mentioned in Sentence1."
+                    }]
+                }
+            )
+        }, 
+        {
             "input": "Input1: “At the invitation of Chinese President Xi Jinping, Vladimir Putin will pay a state visit to China on May 16 to 17 as his first foreign trip after taking office,” the Kremlin said.\n\n" +
             "Input2: “At the invitation of Chinese President Xi Jinping, Vladimir Putin will pay a state visit to England on May 1 to 2 as his first foreign trip after taking office,” the Kremlin said.\n\n" +
             json.dumps (
--- flow/personexamples.py
+++ flow/personexamples.py
@@ -14,7 +14,7 @@ def person_examples():
             "output": "Input1: A woman was charged on Thursday (May 9) with the murder of a 56-year-old man at a condominium along Beach Road. Li Ye, a 37-year-old Chinese national, is accused of causing the death of Lim Lai Guan in a unit at City Gate Residences at about 1.40am on May 8. Li was arrested at the scene and a knife was seized.\n\n" +
             "Input2: A man was charged on Thursday (May 9) with the murder of a 56-year-old man at a condominium along Beach Road. Xiao Ming, a 40-year-old Burmese national, is accused of causing the death of Lim Lai Guan in a unit at City Gate Residences at about 1.40am on May 8. Li was arrested at the scene and a knife was seized.\n\n" +
             json.dumps(
-                {"Person": [
+                {"Person Description": [
                     {"Sentence1": "A woman was charged on Thursday (May 9) with the murder of a 56-year-old man at a condominium along Beach Road.",
                     "Sentence2": "A man was charged on Thursday (May 9) with the murder of a 56-year-old man at a condominium along Beach Road.",
                     "Reason": "The accused in Sentence1 is a woman, but in Sentence2, it is a man",
@@ -58,7 +58,7 @@ def person_examples():
                     "Sentence2": "A 39-year-old man who was arrested earlier this week after allegedly attempting to flee from the police and having a gun in his van, was charged in a district court on July 2.",
                     "Reason": "The object found in the vehicle is a samurai sword in Sentence1, but in Sentence2, it is a gun. Also, the type of vehicle in Sentence1 is a car, but in Sentence2, it is a van."
                     }],
-                "Person": [
+                "Person Description": [
                     {"Sentence1": "A 22-year-old man who was arrested earlier this week after allegedly attempting to flee from the police and having a samurai sword in his car, was charged in a district court on May 17.",
                     "Sentence2": "A 39-year-old man who was arrested earlier this week after allegedly attempting to flee from the police and having a gun in his van, was charged in a district court on July 2.",
                     "Reason": "The age of the man in Sentence2 (39 years old) does not match the age in Sentence1 (22 years old)"
