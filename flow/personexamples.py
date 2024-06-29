8e7112605cc1424f7d85f3d3fd2cdec2f3527171
kkaiyu <127333335+kkaiyu@users.noreply.github.com>
2024-06-30 01:47:11 +0800

    standardise examples

--- flow/personexamples.py
+++ flow/personexamples.py
@@ -17,7 +17,7 @@ def person_examples():
                 {"Person Description": [
                     {"Sentence1": "Kwek got to know her lover and co-accused Lai Sze Yin in July 2016, when the 24-year-old man delivered a parcel to her home.",
                     "Sentence2": "Kwek got to know Lai Sze Yin, 28, and they entered a romantic relationship.",
-                    "Reason": "Lai Sze Yin is a 24-year-old man in Sentence1, but in Sentence2, he is 28 years old",
+                    "Reason": "Lai Sze Yin is a 24-year-old man in Sentence1, but in Sentence2, he is 28 years old.",
                     },
                 ]
                 }
@@ -32,12 +32,12 @@ def person_examples():
                 {"Person Description": [
                     {"Sentence1": "A woman was charged on Thursday (May 9) with the murder of a 56-year-old man at a condominium along Beach Road.",
                     "Sentence2": "A man was charged on Thursday (May 9) with the murder of a 56-year-old man at a condominium along Beach Road.",
-                    "Reason": "The accused in Sentence1 is a woman, but in Sentence2, it is a man",
+                    "Reason": "The accused in Sentence1 is a woman, but in Sentence2, it is a man.",
                     },
 
                     {"Sentence1": " Li Ye, a 37-year-old Chinese national, is accused of causing the death of Lim Lai Guan in a unit at City Gate Residences at about 1.40am on May 8.",
                     "Sentence2": "Xiao Ming, a 40-year-old Burmese national, is accused of causing the death of Lim Lai Guan in a unit at City Gate Residences at about 1.40am on May 8.",
-                    "Reason": "The name, age and nationaity of the accused in Sentence2 (Xiao Ming, 40 years old, Burmese) does not match the name, age and nationality of the accused in Sentence1 (Li Ye, 37 years old, Chinese)"
+                    "Reason": "The name, age and nationaity of the accused in Sentence2 (Xiao Ming, 40 years old, Burmese) does not match the name, age and nationality of the accused in Sentence1 (Li Ye, 37 years old, Chinese)."
                     }
                 ]
                 }
@@ -66,7 +66,7 @@ def person_examples():
                 "Person Description": [
                     {"Sentence1": "A 22-year-old man who was arrested earlier this week after allegedly attempting to flee from the police and having a samurai sword in his car, was charged in a district court on May 17.",
                     "Sentence2": "A 39-year-old man who was arrested earlier this week after allegedly attempting to flee from the police and having a gun in his van, was charged in a district court on July 2.",
-                    "Reason": "The age of the man in Sentence2 (39 years old) does not match the age in Sentence1 (22 years old)"
+                    "Reason": "The age of the man in Sentence2 (39 years old) does not match the age in Sentence1 (22 years old)."
                     }], 
                 }
             ),
